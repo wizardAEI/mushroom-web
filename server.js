6fdfc68fd5b111ac8a892d77c0b22b05fee2d49f
wangdejiang <wangdejiang@baidu.com>
2024-10-28 19:08:21 +0800

    feat: sandbox

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 3003;
 
 // 设置静态资源目录
-app.use(express.static('public'));
-app.use('/test', express.static('public'));
+app.use(express.static("public"));
+app.use("/test", express.static("public"));
+app.use("/sandbox", express.static("public"));
 
 app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
+  console.log(`Server running at http://localhost:${port}`);
 });
